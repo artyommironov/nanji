d7502774f2822af910e3091872637247dc71f4f2
Artyom Mironov <artyommironov@protonmail.com>
2022-10-30 19:42:03 +0900

    Refactor AppearanceActivity

--- app/src/main/java/com/kazufukurou/nanji/ui/AppearanceActivity.kt
+++ app/src/main/java/com/kazufukurou/nanji/ui/AppearanceActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.Menu
 import android.widget.SeekBar
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import com.artyommironov.colorpicker.ColorPicker
 import com.artyommironov.colorpicker.ColorTextWatcher
@@ -16,49 +17,47 @@ import com.kazufukurou.nanji.model.getPrefs
 import kotlin.properties.Delegates
 
 class AppearanceActivity : AppCompatActivity() {
+  private val binding: AppearanceBinding by lazy { AppearanceBinding.inflate(layoutInflater) }
   private val colorTextWatcher by lazy { ColorTextWatcher(binding.colorPicker) }
   private val prefs by lazy { getPrefs() }
-  private var isText by Delegates.observable(false) { _, old, new -> if (new != old) init() }
-  private var textColor by Delegates.observable(0) { _, old, new -> if (new != old) render() }
-  private var bgColor by Delegates.observable(0) { _, old, new -> if (new != old) render() }
-  private var cornerRadius by Delegates.observable(0) { _, old, new -> if (new != old) render() }
-  private var textSize by Delegates.observable(0) { _, old, new -> if (new != old) render() }
-  private var fullWidthCharacters by Delegates.observable(false) { _, old, new -> if (new != old) render() }
-  private lateinit var binding: AppearanceBinding
+  private var state: State by Delegates.observable(State()) { _, old, new ->
+    if (new != old) {
+      if (new.isText != old.isText) {
+        updatePickers()
+      }
+      render()
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = AppearanceBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    bgColor = prefs.bgColor
-    textColor = prefs.textColor
-    cornerRadius = prefs.cornerRadius.coerceIn(prefs.cornerRadiusRange)
-    textSize = prefs.textSize.coerceIn(prefs.textSizeRange)
-    fullWidthCharacters = prefs.fullWidthCharacters
+    state = getInitialState()
 
     binding.viewSampleBg.background = SquareTileDrawable(resources.dp(8), Color.WHITE, Color.LTGRAY)
-    binding.switchWideText.setOnClickListener { fullWidthCharacters = !fullWidthCharacters }
-    binding.buttonBg.setOnClickListener { isText = false }
-    binding.buttonText.setOnClickListener { isText = true }
+    binding.switchWideText.setOnClickListener { state = state.copy(fullWidthCharacters = !state.fullWidthCharacters) }
+    binding.buttonBg.setOnClickListener { state = state.copy(isText = false) }
+    binding.buttonText.setOnClickListener { state = state.copy(isText = true) }
     with(binding.seekTextSize) {
       val min = prefs.textSizeRange.first
       max = prefs.textSizeRange.last - min
-      onProgressChange { fromUser, progress -> if (fromUser) textSize = min + progress }
+      onProgressChange { fromUser, progress -> if (fromUser) state = state.copy(textSizeDp = min + progress) }
     }
     with(binding.seekCornerRadius) {
       val min = prefs.cornerRadiusRange.first
       max = prefs.cornerRadiusRange.last - min
-      onProgressChange { fromUser, progress -> if (fromUser) cornerRadius = min + progress }
+      onProgressChange { fromUser, progress -> if (fromUser) state = state.copy(cornerRadiusDp = min + progress) }
     }
     binding.colorPicker.setup(
       mode = ColorPicker.Mode.RGBA,
       onColorChange = {
         val color = binding.colorPicker.color
-        if (isText) textColor = color else bgColor = color
+        state = if (state.isText) state.copy(textColor = color) else state.copy(bgColor = color)
       }
     )
-    init()
+    updatePickers()
+    render()
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -71,43 +70,52 @@ class AppearanceActivity : AppCompatActivity() {
     save()
   }
 
-  private fun init() {
-    binding.seekCornerRadius.progress = cornerRadius - prefs.cornerRadiusRange.first
-    binding.seekTextSize.progress = textSize - prefs.textSizeRange.first
-    binding.colorPicker.color = if (isText) textColor else bgColor
-    render()
+  private fun updatePickers() = with(binding) {
+    seekCornerRadius.progress = state.cornerRadiusDp - prefs.cornerRadiusRange.first
+    seekTextSize.progress = state.textSizeDp - prefs.textSizeRange.first
+    colorPicker.color = if (state.isText) state.textColor else state.bgColor
   }
+
+  private fun getInitialState() = State(
+    isText = false,
+    bgColor = prefs.bgColor,
+    textColor = prefs.textColor,
+    textSizeDp = prefs.textSize.coerceIn(prefs.textSizeRange),
+    cornerRadiusDp = prefs.cornerRadius.coerceIn(prefs.cornerRadiusRange),
+    fullWidthCharacters = prefs.fullWidthCharacters,
+  )
 
   private fun render() {
     colorTextWatcher.updateEditText(binding.editColor)
-    val textSize = resources.dp(textSize).toFloat()
-    val radius = resources.dp(cornerRadius).toFloat()
+    val textSize = resources.dp(state.textSizeDp).toFloat()
+    val radius = resources.dp(state.cornerRadiusDp).toFloat()
     with(binding.textSample) {
-      text = if (fullWidthCharacters) "１２" else "12"
-      setTextColor(textColor)
+      text = if (state.fullWidthCharacters) "１２" else "12"
+      setTextColor(state.textColor)
       setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
       background = GradientDrawable().also {
-        it.setColor(bgColor)
+        it.setColor(state.bgColor)
         it.cornerRadius = radius
         it.setBounds(0, 0, width, height)
       }
     }
-    binding.switchWideText.isChecked = fullWidthCharacters
-    binding.buttons.check((if (isText) binding.buttonText else binding.buttonBg).id)
+    binding.switchWideText.isChecked = state.fullWidthCharacters
+    binding.buttons.check((if (state.isText) binding.buttonText else binding.buttonBg).id)
   }
 
   private fun reset() {
-    isText = false
     prefs.clearAppearance()
-    init()
+    state = getInitialState()
+    updatePickers()
+    render()
   }
 
-  private fun save() {
-    prefs.bgColor = bgColor
-    prefs.textColor = textColor
-    prefs.cornerRadius = cornerRadius
-    prefs.textSize = textSize
-    prefs.fullWidthCharacters = fullWidthCharacters
+  private fun save() = with(prefs) {
+    bgColor = state.bgColor
+    textColor = state.textColor
+    cornerRadius = state.cornerRadiusDp
+    textSize = state.textSizeDp
+    fullWidthCharacters = state.fullWidthCharacters
   }
 
   private fun SeekBar.onProgressChange(action: (fromUser: Boolean, progress: Int) -> Unit) {
@@ -119,4 +127,13 @@ class AppearanceActivity : AppCompatActivity() {
       }
     )
   }
+
+  private data class State(
+    val isText: Boolean = false,
+    @ColorInt val textColor: Int = Color.TRANSPARENT,
+    @ColorInt val bgColor: Int = Color.TRANSPARENT,
+    val textSizeDp: Int = 0,
+    val cornerRadiusDp: Int = 0,
+    val fullWidthCharacters: Boolean = false,
+  )
 }
