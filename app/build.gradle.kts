520f578bb7ffb73fa70b35b1c91b5908f2802955
Artyom Mironov <artyommironov@protonmail.com>
2023-03-26 14:26:29 +0900

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,16 +41,16 @@ android {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
   implementation("com.github.artyommironov:anyadapter:1.0.1")
-  implementation("com.google.android.material:material:1.7.0")
+  implementation("com.google.android.material:material:1.8.0")
   implementation("androidx.core:core-ktx:1.9.0")
-  implementation("androidx.appcompat:appcompat:1.5.1")
-  implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
   implementation("androidx.preference:preference:1.2.0")
-  implementation("androidx.recyclerview:recyclerview:1.2.1")
+  implementation("androidx.recyclerview:recyclerview:1.3.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.20")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.10")
 }
