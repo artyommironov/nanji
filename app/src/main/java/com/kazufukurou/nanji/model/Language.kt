c0a860da1f213fd806be27bf6a80bf8060bb880e
inson1 <75314629+inson1@users.noreply.github.com>
2023-08-20 21:37:43 +0200

    Create Language.kt

---
+++ app/src/main/java/com/kazufukurou/nanji/model/Language.kt
@@ -0,0 +1,15 @@
+package com.kazufukurou.nanji.model
+
+import androidx.annotation.StringRes
+import com.kazufukurou.nanji.R
+
+enum class Language(@StringRes val title: Int) {
+  system(R.string.languageSystem),
+  zhCN(R.string.languageZhCn),
+  zhTW(R.string.languageZhTw),
+  ja(R.string.languageJa),
+  ko(R.string.languageKo),
+  ru(R.string.languageRu),
+  en(R.string.languageEn)
+  cz(R.string.languageCz)
+}
